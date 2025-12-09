254519c2e359eeb5102e86959c32b3bb7ab5b274
Christopher Amah <christopheramah0@gmail.com>
2025-12-09 11:10:47 +0100

    organogram scroll

--- src/components/Organogram.jsx
+++ src/components/Organogram.jsx
@@ -3,16 +3,22 @@ import org from '../assets/org.png';
 
 const OurFoundation = () => {
   return (
-    <section className="py-8 md:py-16 px-4 md:px-8  lg:px-20">
+    <section className="py-8 md:py-16 px-4 md:px-8 lg:px-20">
       <div className="max-w-5xl mx-auto space-y-8">
         {/* Header */}
         <h1 className="text-3xl sm:text-5xl font-medium text-center text-[#03045E] leading-tight mb-8 md:mb-[54px]">
           ORGANOGRAM
         </h1>
 
-        {/* Content */}
-        <div>
-            <img src={org} alt="" />
+        {/* Scrollable Content */}
+        <div className="overflow-x-auto">
+          <div className="min-w-[600px] md:min-w-full">
+            <img
+              src={org}
+              alt="Organogram"
+              className="w-full h-auto object-contain"
+            />
+          </div>
         </div>
       </div>
     </section>
